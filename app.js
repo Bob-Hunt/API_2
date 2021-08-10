d2d1d22b8ca26e27fe80916555a3772abc8d2751
BobHunt <bomazani@icloud.com>
2021-08-09 23:10:19 -0400

    Countries worked all the way to line#182. The data is now an array of countries, so there is no object property to filter through lines 184-223.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// const apiKey = "47a456ff-4d8c-4295-b487-9500e5c8cb5e";
+const apiKey = "47a456ff-4d8c-4295-b487-9500e5c8cb5e";
 // let endpoint;
 // let country;
 // let state;
@@ -12,10 +12,6 @@
 // let badAirList3 = document.getElementById('yuck3');
 // let badAirList4 = document.getElementById('yuck4');
 
-
-
-
-
 // Minimized fetches //
 async function getCountries(){
     console.log(`Getting Countries!`);
@@ -23,8 +19,15 @@ async function getCountries(){
     .then(function(response){
         return response.json();
     })
-    .then((data) => {
-        return data
+    .then((json) => {
+        console.log(json.data);
+        let dataArr = [];
+        for (let dataItem of json.data){
+            dataArr.push(dataItem.country);
+        };
+        console.log(`completed the loop!`)
+        continueFunction(dataArr);
+    
     })
     .catch((err) => {
         console.log(err);
@@ -37,8 +40,15 @@ async function getStates(country){
     .then(function(response){
         return response.json();
     })
-    .then((data) => {
-        return data
+    .then((json) => {
+        console.log(json.data);
+        let dataArr = [];
+        for (let dataItem of json.data){
+            dataArr.push(dataItem.state);
+        };
+        console.log(`completed the loop!`)
+        continueFunction(dataArr);
+    
     })
     .catch((err) => {
         console.log(err);
@@ -51,8 +61,15 @@ async function getCities(country, state){
     .then(function(response){
         return response.json();
     })
-    .then((data) => {
-        return data
+    .then((json) => {
+        console.log(json.data);
+        let dataArr = [];
+        for (let dataItem of json.data){
+            dataArr.push(dataItem.city);
+        };
+        console.log(`completed the loop!`)
+        continueFunction(dataArr);
+    
     })
     .catch((err) => {
         console.log(err);
@@ -65,8 +82,16 @@ async function getCityInfo(country, state, city){
     .then(function(response){
         return response.json();
     })
-    .then((data) => {
-        return data
+    .then((json) => {
+        console.log(json.data);
+        let dataArr = [];
+        for (let dataItem of json.data){
+            dataArr.push(dataItem.location);
+            dataArr.push(dataItem.current);
+        };
+        console.log(`completed the loop!`)
+        continueFunction(dataArr);
+    
     })
     .catch((err) => {
         console.log(err);
@@ -88,19 +113,24 @@ let displayThree = document.getElementById('ul-three');
 // accepts an event from 1 of 4 different HTML buttons and calls getFetch()
 // with different parameters based on which button is clicked.
 function chooseInput(mybutton){
+    console.log(`Starting chooseInput function`);
 
     console.log(mybutton.id);
     switch (mybutton.id) {
-        case 'countries':
+        case 'states':
+            console.log(`case: states`);
             getStates(country);
             break;
         case 'cities':
+            console.log(`case: cities`);
             getCities(country, state);
             break;
         case 'city_data':
+            console.log(`case: city_data`);
             getCityInfo(country, state, city);
             break;
         default:
+            console.log(`case: nuthin'`);
             getCountries();
             break;
     }
@@ -111,24 +141,25 @@ function chooseInput(mybutton){
 //********************************
 //**** The function below would be one of the four get/Country/State/City/CityData fetches
 async function getFetch(countryInput, stateInput, cityInput){
-// function getFetch(countryInput, stateInput, cityInput){
-    if (cityInput !== null){
-        getCityInfo(countryInput, stateInput, cityInput);
-    } else if (stateInput !== null) {
-        getCities(countryInput, stateInput);
-    } else if (countryInput !== null) {
-        getStates(countryInput);
-    } else {
+    console.log(`starting getFetch`)
+    if (countryInput !== null) {
         getCountries();
+    } else if (statesInput !== null) {
+        getStates(countryInput);
+    } else if (cityInput !== null){        getCities(countryInput, stateInput,);
+        getCities(countryInput, stateInput);
+    } else {
+        getCityData(countryInput, stateInput, cityInput);
     };
-    // await fetch(myParameter)
-    //     .then(function(response){
-    //         return response.json();
-    //     })
-    //     .then((data) => {
-    console.log(data.length);
-    console.log(data);
-
+};
+    // console.log(`Back to getFetch! Woo!`)
+    // console.log(data.length);
+    // console.log(data);
+    // let myData = data;
+    // console.log(`myData: ${myData}`)
+async function continueFunction(dataArr){
+    console.log(`continueFunction`);
+    console.log(dataArr);
     // Removes results from previous button before adding results from current button.
     while (displayOne.firstChild) {
         displayOne.removeChild(displayOne.firstChild);
@@ -139,9 +170,12 @@ async function getFetch(countryInput, stateInput, cityInput){
     while (displayThree.firstChild) {
         displayThree.removeChild(displayThree.firstChild);
     };
-
-    let count = 0
-    for (item of data){
+    console.log(`Finished getting rid of the children!`)    
+    
+    let count = 0;
+    console.log(count);
+    for (item of dataArr){
+        console.log(item);
         count += 1;
         console.log(count);
         let listItem = document.createElement('li');                // CityData has keys of "current" & "location", which have their own keys. 
@@ -150,26 +184,32 @@ async function getFetch(countryInput, stateInput, cityInput){
         if (item.hasOwnProperty('country')) {
             listItem.innerHTML = '<p>' + item.country + '</p>';
             bodyWrapperImg.setAttribute('id', 'country');
+            break;
         
         // checks for key of 'state', if exists, uses 'state' for the key.
         } else if (item.hasOwnProperty('state')) {
             listItem.innerHTML = '<p>' + item.country + '</p>';
             bodyWrapperImg.setAttribute('id', 'state');
+            break;
 
         // checks for key of 'city', if exists, uses 'city' for the key.
         } else if (item.hasOwnProperty('city')) {
             listItem.innerHTML = '<p>' + item.country + '</p>';
             bodyWrapperImg.setAttribute('id', 'city');
+            break;
 
         // checks for key of 'location' or 'current', if exists, uses 'cityData' for the key.
         } else if (item.hasOwnProperty('location')) {
             listItem.innerHTML = '<p>' + item.country + '</p>';
             bodyWrapperImg.setAttribute('id', 'cityDataLocation');
+            break;
         } else if (item.hasOwnProperty('current')) {
             listItem.innerHTML = '<p>' + item.country + '</p>';
             bodyWrapperImg.setAttribute('id', 'cityDataCurrent');
+            break;
         } else { 
             console.log(`Item didn't belong`);
+            break;
         }
 
         //  Based on each item's 'count', this assigns the item to one of 3 columns
@@ -181,168 +221,5 @@ async function getFetch(countryInput, stateInput, cityInput){
             displayItems = document.querySelector('#ul-one');
         }
     }
-    displayItems.appendChild(listItem);  
+    // displayItems.appendChild(listItem);  
 };
-
-
-
-
-
-// // Ghibli Code //
-// // URL variables
-// const baseUrl = 'https://ghibliapi.herokuapp.com/';
-// const filmsUrl = `${baseUrl}films`;
-// const peopleUrl = `${baseUrl}people`;
-// const speciesUrl = `${baseUrl}species`;
-// const locationsUrl = `${baseUrl}locations`;
-// const vehiclesUrl = `${baseUrl}vehicles`;
-
-// let myUrl;
-// let english = true;
-// let showHideDiv = document.querySelector('.translate-container');
-// showHideDiv.setAttribute('id', 'hide-translate-container');
-
-// let displayItems;
-// let bodyWrapperImg = document.querySelector(".body-wrapper-image");
-// let displayOne = document.getElementById('ul-one');
-// let displayTwo = document.getElementById('ul-two');
-// let displayThree = document.getElementById('ul-three');
-
-
-// // accepts an event from 1 of 5 different HTML buttons and calls getFetch()
-// // with different parameters based on which button is clicked.
-// function chooseInput(mybutton, language){
-//     console.log(mybutton.id);
-//     switch (mybutton.id) {
-//         case 'films':
-//             getFetch(filmsUrl, 'english');
-//             break;
-//         case 'people':
-//             getFetch(peopleUrl, 'people');
-//             break;
-//         case 'species':
-//             getFetch(speciesUrl, 'species');
-//             break;
-//         case 'locations':
-//             getFetch(locationsUrl, 'locations');
-//             break;
-//         case 'vehicles':
-//             getFetch(vehiclesUrl, 'vehicles');
-//             break;
-//         default:
-//             break;
-//     }
-// }
-
-
-// // Determines the second parameter to pass into getFetch(),
-// // which will determine which key:value will be pulled from the films API.
-// function titleTranslate(transBtn){
-//     switch (transBtn.id) {
-//         case 'english':
-//             getFetch(filmsUrl, 'english');
-//             break;
-//         case 'japanese':
-//             getFetch(filmsUrl, 'japanese');
-//             break;
-//         default:
-//             break;
-//     }
-// }
-
-
-// // getFetch function **Does way too much!
-// // Should be refactored into multiple functions.
-// async function getFetch(myParameter, language){   
-//     await fetch(myParameter)
-//         .then(function(response){
-//             return response.json();
-//         })
-//         .then((data) => {
-//             console.log(data.length);
-//             console.log(data);
-
-//             // Removes results from previous button before adding results from current button.
-//             while (displayOne.firstChild) {
-//                 displayOne.removeChild(displayOne.firstChild);
-//             };
-//             while (displayTwo.firstChild) {
-//                 displayTwo.removeChild(displayTwo.firstChild);
-//             };
-//             while (displayThree.firstChild) {
-//                 displayThree.removeChild(displayThree.firstChild);
-//             };
-
-//             let count = 0
-//             for (item of data){
-//                 count += 1;
-//                 let listItem = document.createElement('li');
-//                 // Films has a key of 'title' instead of 'name', 
-//                 // checks for key of 'title', if exists, uses 'title' key instead of 'name' key.
-//                 if (item.hasOwnProperty('title')) {
-//                     if (language === 'english'){
-//                         listItem.innerHTML = '<p>' + item.title + '</p>';
-//                         bodyWrapperImg.style.backgroundImage ='url("./Assets/chihiro036.jpeg")';
-//                         bodyWrapperImg.setAttribute('id', 'films-en');
-
-//                     } else if (language === 'japanese'){
-//                         listItem.innerHTML = `<p>${item.original_title} (${item.original_title_romanised})</p>`;
-//                         bodyWrapperImg.style.backgroundImage = 'url("./Assets/nausicaa009.jpeg")';
-//                         bodyWrapperImg.setAttribute('id', 'films-jp');
-//                     }
-//                     } else if (language === 'romanji'){
-//                         listItem.innerHTML = '<p>' + item.original_title_romanised +  '</p>';
-//                         bodyWrapperImg.style.backgroundImage = 'url("./Assets/chihiro036.jpeg")';
-//                         bodyWrapperImg.setAttribute('id', 'films-en');
-//                 } else {
-//                     listItem.innerHTML = '<p>' + item.name + '</p>';
-//                     switch (language){
-//                         case 'people':
-//                             bodyWrapperImg.style.backgroundImage = 'url("./Assets/nausicaa049.jpeg")';
-//                             bodyWrapperImg.setAttribute('id', 'people');
-//                             break;
-//                         case 'species':
-//                             bodyWrapperImg.style.backgroundImage = 'url("./Assets/totoro024.jpeg")';
-//                             bodyWrapperImg.setAttribute('id', 'species');
-//                             break;
-//                         case 'locations':
-//                             bodyWrapperImg.style.backgroundImage = 'url("./Assets/chihiro011.jpeg")';
-//                             bodyWrapperImg.setAttribute('id', 'locations');
-//                             break;
-//                         case 'vehicles':
-//                             bodyWrapperImg.style.backgroundImage = 'url("./Assets/ponyo039.jpeg")';
-//                             bodyWrapperImg.setAttribute('id', 'vehicles');
-//                             break;
-//                     }
-//                 }
-   
-//                 //  Based on each item's 'count' it assigns it to one of 3 columns
-//                 if (count %3 === 0){
-//                     displayItems = document.querySelector('#ul-three');
-//                 } else if (count %3 === 2){
-//                     displayItems = document.querySelector('#ul-two');
-//                 } else if (count %3 === 1){
-//                     displayItems = document.querySelector('#ul-one');
-//                 }
-//                 displayItems.appendChild(listItem);  
-//             };
-
-//             switch(language){
-//                 case 'english':
-//                     showHideDiv.setAttribute('id', 'show-translate-container');
-//                     break;
-//                 case 'japanese':
-//                     showHideDiv.setAttribute('id', 'show-translate-container');
-//                     break;
-//                 case 'romanji':
-//                     showHideDiv.setAttribute('id', 'show-translate-container');
-//                     break;
-//                 default:
-//                     showHideDiv.setAttribute('id', 'hide-translate-container');
-//                     break;
-//                 }
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         })
-// }
